637425c3aba853269b36459d7e8cc239c42410d2
Hunter Cox <huntersamuelcox1992@gmail.com>
2023-07-26 07:39:54 -0400

    Add Spatie package for managing user roles and permissions. Added 3 roles; customer, admin and super-admin.

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,7 +3,12 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+use App\Models\User;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\File;
+use Illuminate\Support\Facades\Hash;
+use Spatie\Permission\Models\Role;
 
 class DatabaseSeeder extends Seeder
 {
@@ -12,11 +17,16 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // \App\Models\User::factory(10)->create();
+        $this->call([
+            RolePermissionSeeder::class,
+        ]);
 
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        $user = User::create([
+            'nick' => 'Hunter',
+            'email' => 'hunter@laravel.com',
+            'password' => Hash::make('hunter@laravel.com')
+        ]);
+
+        $user->assignRole(Role::findByName('Super-Admin'));
     }
 }
---
+++ database/seeders/RolePermissionSeeder.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Spatie\Permission\Models\Permission;
+use Spatie\Permission\Models\Role;
+use Spatie\Permission\PermissionRegistrar;
+
+class RolePermissionSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        app()[PermissionRegistrar::class]->forgetCachedPermissions();
+        // create permissions
+        Permission::create(['name' => 'add_products']);
+        Permission::create(['name' => 'delete_products']);
+        Permission::create(['name' => 'rearrange_menu']);
+
+
+        // create roles and assign existing permissions
+        $role1 = Role::create(['name' => 'customer']);
+
+        $role2 = Role::create(['name' => 'admin']);
+        $role2->givePermissionTo('add_products');
+        $role2->givePermissionTo('delete_products');
+        $role2->givePermissionTo('rearrange_menu');
+
+        $role3 = Role::create(['name' => 'Super-Admin']);
+
+        // create demo users
+        $user = \App\Models\User::factory()->create([
+            'name' => 'Example Customer User',
+            'email' => 'test@example.com',
+        ]);
+        $user->assignRole($role1);
+
+        $user = \App\Models\User::factory()->create([
+            'name' => 'Example Admin User',
+            'email' => 'admin@example.com',
+        ]);
+        $user->assignRole($role2);
+
+        $user = \App\Models\User::factory()->create([
+            'name' => 'Example Super-Admin User',
+            'email' => 'superadmin@example.com',
+        ]);
+        $user->assignRole($role3);
+    }
+}
