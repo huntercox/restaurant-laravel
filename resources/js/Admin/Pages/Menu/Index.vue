7f2df5d60b77fd24273add4d8fb0c58557d16a47
Hunter Cox <huntersamuelcox1992@gmail.com>
2023-07-31 07:13:50 -0400

    Got menus showing up on Index Vue component.

--- resources/js/Admin/Pages/Menu/Index.vue
+++ resources/js/Admin/Pages/Menu/Index.vue
@@ -2,7 +2,7 @@
 import { Head, Link } from '@inertiajs/vue3';
 import AuthenticatedLayout from '@/Admin/Layouts/AuthenticatedLayout.vue';
 
-
+defineProps(['menus']);
 </script>
 
 <template>
@@ -13,6 +13,10 @@ import AuthenticatedLayout from '@/Admin/Layouts/AuthenticatedLayout.vue';
 
         <Link :href="route('admin.menus.create')">Create Menu</Link>
 
-
+        <div class="max-w-7xl mx-auto px-4 sm-px-6 lg-px-8">
+            <div v-for="menu in menus" :key="menu.id" :menu="menu">
+                Menu Name: {{ menu.name }}
+            </div>
+        </div>
     </AuthenticatedLayout>
 </template>
