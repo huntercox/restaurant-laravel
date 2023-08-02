30d1e38a7289d7ddc7cbc6c846565bde1731dde7
Hunter Cox <huntersamuelcox1992@gmail.com>
2023-08-02 08:16:56 -0400

    Apply menu index styles to item index page.

--- resources/js/Admin/Pages/Item/Index.vue
+++ resources/js/Admin/Pages/Item/Index.vue
@@ -21,8 +21,12 @@ defineProps(['items']);
 
 
         <div class="max-w-7xl mx-auto px-4 sm-px-6 lg-px-8">
-            <div v-for="item in items" :key="item.id" :item="item">
-                Item Name: {{ item.name }}
+            <div class="flex items-center justify-start gap-2 p-3">
+                <div v-for="item in items" :key="item.id" :item="item" class="border border-red-600 p-2 px-3">
+                    <p class="font-black text-lg">{{ item.name }}</p>
+                    <p class="text-xs">{{ item.description }}</p>
+                    <p class="text-right mt-4 font-bold text-sm">$ {{ item.price }}</p>
+                </div>
             </div>
         </div>
     </AuthenticatedLayout>
