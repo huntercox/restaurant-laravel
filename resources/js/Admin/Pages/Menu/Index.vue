863fe1f7d28da436e7b12a1d554a1e1384b2a23d
Hunter Cox <huntersamuelcox1992@gmail.com>
2023-08-02 08:15:13 -0400

    Added some more tailwind styles to the menus index page.

--- resources/js/Admin/Pages/Menu/Index.vue
+++ resources/js/Admin/Pages/Menu/Index.vue
@@ -15,20 +15,26 @@ defineProps(['menus']);
 
                 <h1 class="text-xl font-black">Menus</h1>
 
-                <Link class="rounded-md bg-red-600 p-2" :href="route('admin.menus.create')">Create Menu</Link>
+                <Link class="rounded-md bg-red-700 p-2 px-4 text-white font-black" :href="route('admin.menus.create')">
+                Create Menu</Link>
             </div>
         </div>
 
         <div class="max-w-7xl mx-auto px-4 sm-px-6 lg-px-8">
 
-            <div class="flex gap-40 justify-center">
-                <div class="w-full bg-gray-200 p-3" v-for=" menu in menus" :key="menu.id" :menu="menu">
-                    <p class="font-black border-b-2 mb-2 border-b-red-600">{{ menu.name }}</p>
+            <div class="flex gap-4 justify-center">
+                <div class="w-full bg-gray-200" v-for=" menu in menus" :key="menu.id" :menu="menu">
+                    <div class="bg-red-600 text-white uppercase p-3">
+                        <p class="font-black">{{ menu.name }}</p>
+                    </div>
 
-                    <div v-for="item in menu.items">
-                        <p>{{ item.name }}</p>
-                        <p>{{ item.description }}</p>
-                        <p>{{ item.price }}</p>
+
+                    <div class="flex items-center justify-start gap-2 p-3">
+                        <div v-for="item in menu.items" class="border border-red-600 p-2 px-3">
+                            <p class="font-black text-lg">{{ item.name }}</p>
+                            <p class="text-xs">{{ item.description }}</p>
+                            <p class="text-right mt-4 font-bold text-sm">$ {{ item.price }}</p>
+                        </div>
                     </div>
                 </div>
             </div>
