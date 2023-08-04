63cd586b9d3f7bc293e74eba0aa58d07824cbe14
Hunter Cox <huntersamuelcox1992@gmail.com>
2023-08-04 07:05:13 -0400

    Add menus/items to menu page on Customer side. Also removed hamburger menu and responsive styling for the user dropdown.

--- resources/js/Customer/Components/Header.vue
+++ resources/js/Customer/Components/Header.vue
@@ -7,84 +7,70 @@ import ResponsiveNavLink from "@/Customer/Components/ResponsiveNavLink.vue";
 </script>
 
 <template>
-  <header>
-    <div>
-      <nav class="bg-red-500 border-b border-gray-100">
-        <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-          <div class="h-16">
-            <div class="flex w-full h-full align-center justify-between">
-              <div class="flex justify-start">
-                <!-- Logo -->
-                <div class="shrink-0 flex items-center justify-between">
-                  <Link href="/">
-                  <ApplicationLogo class="block w-auto fill-current text-gray-800 hover:scale-125 transition-transform" />
-                  </Link>
+    <header>
+        <div>
+            <nav class="bg-red-500 border-b border-gray-100">
+                <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+                    <div class="h-16">
+                        <div class="flex w-full h-full align-center justify-between">
+                            <div class="flex justify-start">
+                                <!-- Logo -->
+                                <div class="shrink-0 flex items-center justify-between">
+                                    <Link href="/">
+                                    <ApplicationLogo
+                                        class="block w-auto fill-current text-gray-800 hover:scale-125 transition-transform" />
+                                    </Link>
+                                </div>
+
+                                <!-- Main Navigation -->
+                                <nav
+                                    class="space-x-8 sm:-my-px sm:ml-10 sm:flex items-center self-center justify-end grow-1">
+                                    <Link :href="route('menu')" :active="route().current()"
+                                        class="text-white font-bold px-2 hover:text-gray-700">
+                                    Menu
+                                    </Link>
+                                </nav>
+                            </div>
+
+                            <template v-if="$page?.props?.auth?.user?.name">
+                                <div class="flex items-center ml-6 justify-end">
+                                    <!-- Settings Dropdown -->
+                                    <div class="ml-3 relative">
+                                        <Dropdown align="right" width="48">
+                                            <template #trigger>
+                                                <span class="inline-flex rounded-md">
+                                                    <button type="button"
+                                                        class="inline-flex items-center px-3 py-2 border border-transparent text-sm leading-4 font-medium rounded-md text-gray-500 bg-white hover:text-gray-700 focus:outline-none transition ease-in-out duration-150">
+                                                        {{ $page?.props?.auth?.user?.name }}
+
+                                                        <svg class="ml-2 -mr-0.5 h-4 w-4" xmlns="http://www.w3.org/2000/svg"
+                                                            viewBox="0 0 20 20" fill="currentColor">
+                                                            <path fill-rule="evenodd"
+                                                                d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z"
+                                                                clip-rule="evenodd" />
+                                                        </svg>
+                                                    </button>
+                                                </span>
+                                            </template>
+
+                                            <template #content>
+                                                <DropdownLink :href="route('profile.edit')"> Profile </DropdownLink>
+                                                <DropdownLink :href="route('auth.logout')" method="post" as="button">
+                                                    Log Out
+                                                </DropdownLink>
+                                            </template>
+                                        </Dropdown>
+                                    </div>
+                                </div>
+                            </template>
+
+                        </div>
+
+
+
+                    </div>
                 </div>
-
-                <!-- Main Navigation -->
-                <nav class="hidden space-x-8 sm:-my-px sm:ml-10 sm:flex items-center self-center justify-start grow-1">
-                  <Link :href="route('menu')" :active="route().current()"
-                    class="text-white font-bold px-2 hover:text-gray-700">
-                  Menu
-                  </Link>
-                </nav>
-              </div>
-
-              <template v-if="$page?.props?.auth?.user?.name">
-                <div class="hidden sm:flex sm:items-center sm:ml-6 justify-end">
-                  <!-- Settings Dropdown -->
-                  <div class="ml-3 relative">
-                    <Dropdown align="right" width="48">
-                      <template #trigger>
-                        <span class="inline-flex rounded-md">
-                          <button type="button"
-                            class="inline-flex items-center px-3 py-2 border border-transparent text-sm leading-4 font-medium rounded-md text-gray-500 bg-white hover:text-gray-700 focus:outline-none transition ease-in-out duration-150">
-                            {{ $page?.props?.auth?.user?.name }}
-
-                            <svg class="ml-2 -mr-0.5 h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"
-                              fill="currentColor">
-                              <path fill-rule="evenodd"
-                                d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z"
-                                clip-rule="evenodd" />
-                            </svg>
-                          </button>
-                        </span>
-                      </template>
-
-                      <template #content>
-                        <DropdownLink :href="route('profile.edit')"> Profile </DropdownLink>
-                        <DropdownLink :href="route('auth.logout')" method="post" as="button">
-                          Log Out
-                        </DropdownLink>
-                      </template>
-                    </Dropdown>
-                  </div>
-                </div>
-              </template>
-            </div>
-
-            <!-- Hamburger -->
-            <div class="-mr-2 flex items-center sm:hidden">
-              <button @click="showingNavigationDropdown = !showingNavigationDropdown"
-                class="inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:bg-gray-100 focus:text-gray-500 transition duration-150 ease-in-out">
-                <svg class="h-6 w-6" stroke="currentColor" fill="none" viewBox="0 0 24 24">
-                  <path :class="{
-                    hidden: showingNavigationDropdown,
-                    'inline-flex': !showingNavigationDropdown,
-                  }" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                    d="M4 6h16M4 12h16M4 18h16" />
-                  <path :class="{
-                    hidden: !showingNavigationDropdown,
-                    'inline-flex': showingNavigationDropdown,
-                  }" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                    d="M6 18L18 6M6 6l12 12" />
-                </svg>
-              </button>
-            </div>
-
-          </div>
+            </nav>
         </div>
-      </nav>
-    </div>
-  </header>
+    </header>
 </template>
