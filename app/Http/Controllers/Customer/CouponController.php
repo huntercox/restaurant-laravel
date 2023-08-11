65533fe3a8ad9bfd832fd570205b5a089587b80a
Hunter Cox <huntersamuelcox1992@gmail.com>
2023-08-11 16:15:03 -0400

    Fix the apply coupon method to account for if a user isn't logged in and has to use the guest_user instead.

--- app/Http/Controllers/Customer/CouponController.php
+++ app/Http/Controllers/Customer/CouponController.php
@@ -4,34 +4,43 @@ namespace App\Http\Controllers\Customer;
 
 use App\Http\Controllers\Controller;
 use App\Models\Coupon;
+use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Session;
 
 class CouponController extends Controller
 {
-    public function apply(Request $request)
-    {
-        $code = $request->input('code');
-        $coupon = Coupon::where('code', $code)->first();
+  public function apply(Request $request)
+  {
+    $code = $request->input('code');
+    $coupon = Coupon::where('code', $code)->first();
 
-      if ($coupon) {
-        // Find the user's cart
-        $user = $request->user();
-        $cart = $user->cart;
-
-        if (!$cart) {
-          return response()->json(['message' => 'No cart found'], 400);
-        }
-
-        // Apply the coupon to the cart
-        $cart->update(['coupon_id' => $coupon->id]);
-
-        // Calculate the new sub_total and total_price
-        $cart->calculate();
-
-        return redirect()->route('checkout')->with('message', 'Coupon applied successfully');
-      }
-
-        return response()->json(['message' => 'Invalid coupon code'], 400);
+    if (!$coupon) {
+      return response()->json(['message' => 'Invalid coupon code'], 400);
     }
+
+    // Find the user's cart or the guest user's cart
+    $user = $request->user();
+    if (!$user) {
+      $guestUserId = Session::get('guest_user_id');
+      if (!$guestUserId) {
+        return response()->json(['message' => 'No user or guest user found'], 400);
+      }
+      $user = User::find($guestUserId);
+    }
+
+    $cart = $user->cart;
+    if (!$cart) {
+      return response()->json(['message' => 'No cart found'], 400);
+    }
+
+    // Apply the coupon to the cart
+    $cart->update(['coupon_id' => $coupon->id]);
+
+    // Calculate the new sub_total and total_price
+    $cart->calculate();
+
+    return redirect()->route('checkout')->with('message', 'Coupon applied successfully');
+  }
 
 }
