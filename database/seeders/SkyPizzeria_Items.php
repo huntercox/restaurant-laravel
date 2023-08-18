5ef3ba5f6821709a2febc5233ec8179ba4fe0709
Hunter Cox <huntersamuelcox1992@gmail.com>
2023-08-17 22:02:50 -0400

    Add more menus to the seeder.

--- database/seeders/SkyPizzeria_Items.php
+++ database/seeders/SkyPizzeria_Items.php
@@ -290,6 +290,297 @@ class SkyPizzeria_Items extends Seeder
         'item_id' => $deep_fried_ravioli_20_beef->id,
       ]);
 
+      // Sky Fries
+
+      $sky_fries_menu = Menu::create(['name' => 'Sky Fries']);
+
+      //plain fries
+      $plain_fries = Item::create([
+        'name' => 'Plain Fries',
+        'description' => "Plain fries.",
+        'price' => round(4.49 * 100),
+      ]);
+      // seasoned fries
+      $seasoned_fries = Item::create([
+        'name' => 'Seasoned Fries',
+        'description' => "Seasoned fries.",
+        'price' => round(5.49 * 100),
+      ]);
+      // cajun fries
+      $cajun_fries = Item::create([
+        'name' => 'Cajun Fries',
+        'description' => "Cajun fries.",
+        'price' => round(5.49 * 100),
+      ]);
+      // ranch seasoned fries
+      $ranch_seasoned_fries = Item::create([
+        'name' => 'Ranch Seasoned Fries',
+        'description' => "Ranch seasoned fries.",
+        'price' => round(5.49 * 100),
+      ]);
+      // bacon cheese fries
+      $bacon_cheese_fries = Item::create([
+        'name' => 'Bacon Cheese Fries',
+        'description' => "Fries with bacon & nacho cheese.",
+        'price' => round(6.49 * 100),
+      ]);
+      // bbq bacon fries
+      $bbq_bacon_fries = Item::create([
+        'name' => 'BBQ Bacon Fries',
+        'description' => "Fries with bacon & BBQ sauce.",
+        'price' => round(6.49 * 100),
+      ]);
+      // loaded fries
+      $loaded_fries = Item::create([
+        'name' => 'Loaded Fries',
+        'description' => "Fries with bacon, onions, and nacho cheese.",
+        'price' => round(6.49 * 100),
+      ]);
+      // extreme loaded fries
+      $extreme_loaded_fries = Item::create([
+        'name' => 'Extreme Loaded Fries',
+        'description' => "Fries with bacon, onions, jalapenos, crushed red peppers, and nacho cheese.",
+        'price' => round(6.49 * 100),
+      ]);
+
+      MenuItem::create([
+        'menu_id' => $sky_fries_menu->id,
+        'item_id' => $plain_fries->id,
+      ]);
+      MenuItem::create([
+        'menu_id' => $sky_fries_menu->id,
+        'item_id' => $seasoned_fries->id,
+      ]);
+      MenuItem::create([
+        'menu_id' => $sky_fries_menu->id,
+        'item_id' => $cajun_fries->id,
+      ]);
+      MenuItem::create([
+        'menu_id' => $sky_fries_menu->id,
+        'item_id' => $ranch_seasoned_fries->id,
+      ]);
+      MenuItem::create([
+        'menu_id' => $sky_fries_menu->id,
+        'item_id' => $bacon_cheese_fries->id,
+      ]);
+      MenuItem::create([
+        'menu_id' => $sky_fries_menu->id,
+        'item_id' => $bbq_bacon_fries->id,
+      ]);
+      MenuItem::create([
+        'menu_id' => $sky_fries_menu->id,
+        'item_id' => $loaded_fries->id,
+      ]);
+      MenuItem::create([
+        'menu_id' => $sky_fries_menu->id,
+        'item_id' => $extreme_loaded_fries->id,
+      ]);
+
+      // Oven Baked 12" Subs
+
+      $subs_menu = Menu::create(['name' => 'Oven Baked 12" Subs']);
+
+      // pepperoni & Sausage sub
+      $pepperoni_sausage_sub = Item::create([
+        'name' => 'Pepperoni & Sausage Sub',
+        'description' => "Pepperoni & Sausage Sub.",
+        'price' => round(9.99 * 100),
+      ]);
+      // five meat sub
+      $five_meat_sub = Item::create([
+        'name' => 'Five Meat Sub',
+        'description' => "Five Meat Sub w/ pepperoni, sausage, ham, bacon, and beef.",
+        'price' => round(9.99 * 100),
+      ]);
+      // sky pizza sub
+      $sky_pizza_sub = Item::create([
+        'name' => 'Sky Pizza Sub',
+        'description' => "Sky Pizza Sub w/ pepperoni, sausage, beef, onions, green peppers, mushrooms and pizza sauce.",
+        'price' => round(9.99 * 100),
+      ]);
+      // ham and cheese sub
+      $ham_cheese_sub = Item::create([
+        'name' => 'Ham & Cheese Sub',
+        'description' => "Ham & Cheese Sub.",
+        'price' => round(9.99 * 100),
+      ]);
+      // chicken bacon ranch sub
+      $chicken_bacon_ranch_sub = Item::create([
+        'name' => 'Chicken Bacon Ranch Sub',
+        'description' => "Grilled chicken breast strips with bacon and ranch sauce.",
+        'price' => round(9.99 * 100),
+      ]);
+      // chicken bacon bbq sub
+      $chicken_bacon_bbq_sub = Item::create([
+        'name' => 'Chicken Bacon BBQ Sub',
+        'description' => "Grilled chicken breast strips with bacon and Sweet Baby Ray's BBQ sauce.",
+        'price' => round(9.99 * 100),
+      ]);
+      // veggie lover's sub
+      $veggie_lovers_sub = Item::create([
+        'name' => 'Veggie Lover\'s Sub',
+        'description' => "Veggie Lover's Sub w/ onions, green peppers, mushrooms, black olives, and banana peppers.",
+        'price' => round(9.99 * 100),
+      ]);
+      // buffalo chicken sub
+      $buffalo_chicken_sub = Item::create([
+        'name' => 'Buffalo Chicken Sub',
+        'description' => "Grilled chicken breast strips with mild or hot buffalo sauce.",
+        'price' => round(9.99 * 100),
+      ]);
+      // chicken parmesan sub
+      $chicken_parmesan_sub = Item::create([
+        'name' => 'Chicken Parmesan Sub',
+        'description' => "Grilled chicken breast strips with marinara sauce and parmesan cheese.",
+        'price' => round(9.99 * 100),
+      ]);
+      // meatball sub
+      $meatball_sub = Item::create([
+        'name' => 'Meatball Sub',
+        'description' => "Meatballs smothered in marinara sauce and cheese.",
+        'price' => round(9.99 * 100),
+      ]);
+      // hot and spicy chicken sub
+      $hot_spicy_chicken_sub = Item::create([
+        'name' => 'Hot & Spicy Chicken Sub',
+        'description' => "Deep fried chicken breast, jalapeno peppers, crushed red peppers, buffalo hot sauce or mild sauce",
+        'price' => round(9.99 * 100),
+      ]);
+
+      MenuItem::create([
+        'menu_id' => $subs_menu->id,
+        'item_id' => $pepperoni_sausage_sub->id,
+      ]);
+      MenuItem::create([
+        'menu_id' => $subs_menu->id,
+        'item_id' => $five_meat_sub->id,
+      ]);
+      MenuItem::create([
+        'menu_id' => $subs_menu->id,
+        'item_id' => $sky_pizza_sub->id,
+      ]);
+      MenuItem::create([
+        'menu_id' => $subs_menu->id,
+        'item_id' => $ham_cheese_sub->id,
+      ]);
+      MenuItem::create([
+        'menu_id' => $subs_menu->id,
+        'item_id' => $chicken_bacon_ranch_sub->id,
+      ]);
+      MenuItem::create([
+        'menu_id' => $subs_menu->id,
+        'item_id' => $chicken_bacon_bbq_sub->id,
+      ]);
+      MenuItem::create([
+        'menu_id' => $subs_menu->id,
+        'item_id' => $veggie_lovers_sub->id,
+      ]);
+      MenuItem::create([
+        'menu_id' => $subs_menu->id,
+        'item_id' => $buffalo_chicken_sub->id,
+      ]);
+      MenuItem::create([
+        'menu_id' => $subs_menu->id,
+        'item_id' => $chicken_parmesan_sub->id,
+      ]);
+      MenuItem::create([
+        'menu_id' => $subs_menu->id,
+        'item_id' => $meatball_sub->id,
+      ]);
+      MenuItem::create([
+        'menu_id' => $subs_menu->id,
+        'item_id' => $hot_spicy_chicken_sub->id,
+      ]);
+
+
+      // Oven Baked Spaghetti
+
+      $spaghetti_menu = Menu::create(['name' => 'Oven Baked Spaghetti']);
+
+      // spaghetti and meatballs
+      $spaghetti_meatballs = Item::create([
+        'name' => 'Spaghetti & Meatballs',
+        'description' => "Spaghetti & Meatballs.",
+        'price' => round(8.59 * 100),
+      ]);
+      // chicken alfredo
+      $chicken_alfredo = Item::create([
+        'name' => 'Chicken Alfredo',
+        'description' => "Chicken Alfredo.",
+        'price' => round(8.99 * 100),
+      ]);
+      // black pepper chicken alfredo
+      $black_pepper_chicken_alfredo = Item::create([
+        'name' => 'Black Pepper Chicken Alfredo',
+        'description' => "Black Pepper Chicken Alfredo.",
+        'price' => round(8.99 * 100),
+      ]);
+      // shrimp and bacon alfredo
+      $shrimp_bacon_alfredo = Item::create([
+        'name' => 'Shrimp & Bacon Alfredo',
+        'description' => "Shrimp & Bacon Alfredo.",
+        'price' => round(9.99 * 100),
+      ]);
+      // chicken parmesan
+      $chicken_parmesan = Item::create([
+        'name' => 'Chicken Parmesan',
+        'description' => "Chicken Parmesan.",
+        'price' => round(9.99 * 100),
+      ]);
+      // pizza spaghetti
+      $pizza_spaghetti = Item::create([
+        'name' => 'Pizza Spaghetti',
+        'description' => "Pizza Spaghetti.",
+        'price' => round(9.99 * 100),
+      ]);
+
+      // Sky Zones
+
+      $zones_menu = Menu::create(['name' => 'Sky Zones']);
+
+      // pepperoni zone
+      $pepperoni_zone = Item::create([
+        'name' => 'Pepperoni Zone',
+        'description' => "Pepperoni Zone.",
+        'price' => round(7.99 * 100),
+      ]);
+      // ham and cheese zone
+      $ham_cheese_zone = Item::create([
+        'name' => 'Ham & Cheese Zone',
+        'description' => "Ham & Cheese Zone.",
+        'price' => round(7.99 * 100),
+      ]);
+      // all meat zone
+      $all_meat_zone = Item::create([
+        'name' => 'All Meat Zone',
+        'description' => "All Meat Zone.",
+        'price' => round(7.99 * 100),
+      ]);
+      // supreme zone
+      $supreme_zone = Item::create([
+        'name' => 'Supreme Zone',
+        'description' => "Supreme Zone.",
+        'price' => round(7.99 * 100),
+      ]);
+
+      MenuItem::create([
+        'menu_id' => $zones_menu->id,
+        'item_id' => $pepperoni_zone->id,
+      ]);
+      MenuItem::create([
+        'menu_id' => $zones_menu->id,
+        'item_id' => $ham_cheese_zone->id,
+      ]);
+      MenuItem::create([
+        'menu_id' => $zones_menu->id,
+        'item_id' => $all_meat_zone->id,
+      ]);
+      MenuItem::create([
+        'menu_id' => $zones_menu->id,
+        'item_id' => $supreme_zone->id,
+      ]);
+
+
 
 
     }
