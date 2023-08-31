5dc0c979083c620af9a71dd05051b97e3e69196a
Hunter Cox <huntersamuelcox1992@gmail.com>
2023-08-31 08:32:22 -0400

    Change the pizza options for Sauce and Crust from checkboxes to radio buttons so only one can be selected per option.

--- resources/js/Customer/Components/Options/PizzaOptions.vue
+++ resources/js/Customer/Components/Options/PizzaOptions.vue
@@ -9,19 +9,39 @@ const selectedCrust = inject('selectedCrust');
 
 <template>
   <div>
-    PizzaOptions.vue
     <div v-for="(option, category) in groupedOptions" :key="category">
-      <h5 class="uppercase font-black">{{ category }}</h5>
-      <div v-for="option in option" :key="option.id" class="grid grid-cols-2 gap-2">
-          <label>
-            <input
-              type="checkbox"
-              v-model="selectedOptions[option.id]"
-            />
-            {{ option.name }} <span class="text-xs italic">(${{ (option.price/100).toFixed(2) }})</span>
-          </label>
+      <div>
+        <h5 class="uppercase font-black bg-red-200 p-1 pl-3 pt-2 mb-2">{{ category }}</h5>
+        <div  class="grid grid-cols-2 gap-2 mb-4">
+          <div v-for="option in option" :key="option.id">
+            <label>
+              <input
+                v-if="category === 'Crust'"
+                type="radio"
+                v-model="selectedCrust"
+                :value="option.id"
+              />
+              <input
+                v-else-if="category === 'Sauce'"
+                type="radio"
+                v-model="selectedSauce"
+                :value="option.id"
+              />
+<!--              <input-->
+<!--                v-else-if="category === 'Toppings'"-->
+<!--                type="checkbox"-->
+<!--                v-model="selectedOptions[option.id]"-->
+<!--              />-->
+              <input
+                v-else
+                type="checkbox"
+                v-model="selectedOptions[option.id]"
+              />
+              {{ option.name }} <span class="text-xs italic">(${{ (option.price/100).toFixed(2) }})</span>
+            </label>
+          </div>
+        </div>
       </div>
     </div>
-
   </div>
 </template>
